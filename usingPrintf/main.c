06b2bbef3edb59631f645142b55b9c07f87a598f
aditya25jain <aditya25051992@gmail.com>
2018-07-28 18:39:49 +0530

    Update main.c

--- usingPrintf/main.c
+++ usingPrintf/main.c
@@ -3,7 +3,6 @@
 /* Standard Includes */
 #include <stdint.h>
 #include <stdbool.h>
-#include <uartInit.h>
 #include <stdio.h>
 #define delay_ms(x)  __delay_cycles((x*4021))
 int main(void)
@@ -13,13 +12,11 @@ int main(void)
     FlashCtl_setWaitState(FLASH_BANK0, 2);
     FlashCtl_setWaitState(FLASH_BANK1, 2);
     PCM_setCoreVoltageLevel(PCM_VCORE1);
-    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_24);
-    uart0Init(115200);
+    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_24);    \\set main DCO frequency to 24MHz
     int counter=0;
     while(1)
     {
         printf("\r\nhello world %d", counter++);
-        uartPrint(EUSCI_A0_BASE, "Hello World debug");
         delay_ms(1000);
         
     }
